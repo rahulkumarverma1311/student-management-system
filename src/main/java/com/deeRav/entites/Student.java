355ae8328bcd9027882265406ffc77ad8c39c704
Rahul Kumar Verma <r.k.vermalnh@gmail.com>
2024-01-11 15:54:02 +0530

    first

--- src/main/java/com/deeRav/entites/Student.java
+++ src/main/java/com/deeRav/entites/Student.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
+@Data   
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
